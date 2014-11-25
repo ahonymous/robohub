6b0f8c9a1d62f38fb77012700cdc8cfc4d64ccfa
Oleksandr Moshta <ahonymous@gmail.com>
2014-11-25 18:03:15 +0200

    temp commit

--- robohub.js
+++ robohub.js
@@ -1,43 +1,59 @@
-function opposite() {
-    var stepType = ['north', 'east', 'south', 'west'];
-    var backStepType = {north: 'south', south: 'north', west: 'east', east: 'west'};
-    var historySteps = [];
-    var steps = [];
-    var reverse = false;
-    var out = true;
+function Robohub() {
+    var directions = [
+            ['east', 'west'],
+            ['north', 'south']
+        ],
+        vocabulary = [
+            '<h1>If you need my help, than click the "HELP" button.</h1>',
+            '<h1>I can help you get out of the maze.</h1>',
+            '<h1>My name is Robik!</h1>',
+            '<h1>Hello!</h1>'
+        ],
+        that = this;
 
-    while (out === true) {
-        var direction = [];
+    function loadMap(robo) {
+        return map().split("\n").filter(function (val) {
+            if (val.length) {
+                return val;
+            }
+        }).map(function (val, indY) {
+            var out = val.split('');
 
-        if (!reverse) {
-            direction = stepType.filter(function (val) {
-                if (isFree(val) && val != backStepType[steps[steps.length - 1]]) {
-                    return val;
-                }
-            });
-        } else {
-            direction = historySteps.pop();
-            window[backStepType[steps.pop()]]();
-        }
+            if (out.indexOf("@") > -1) {
+                robo.startPosition = [out.indexOf("@"), indY];
+            }
 
-        reverse = direction.length < 1;
-        if (steps.length) {
-            direction.sort(function (a) {
-                if (a == steps[steps.length - 1]) {
-                    return 1;
-                }
+            if (out.indexOf("*") > -1) {
+                robo.endPosition = [out.indexOf("*"), indY];
+            }
 
-                return 0;
-            });
-        }
-
-        if (!reverse) {
-            steps.push(direction.pop());
-            historySteps.push(direction);
-            out = window[steps[steps.length - 1]]();
-        }
-        console.log(map());
+            return out;
+        });
     }
 
-    return map();
+    this.speak = function () {
+        setTimeout(function () {
+            if (vocabulary.length) {
+                document.getElementById("out").innerHTML = vocabulary.pop();
+
+                return that.speak();
+            } else {
+                return document.getElementById("help").style.display = '';
+            }
+
+        }, 3000);
+
+    };
+
+    this.step = function () {
+        var steps = [];
+        if (!that.currentMap) {
+            return false;
+        }
+
+
+    };
+
+    this.currentMap = loadMap(this);
+    this.speak();
 }
